30e46c2e7d339bf3bcfc61ac8c1a0c6fe961c20f
sofiamejiamuro <sofiamejiamuro@gmail.com>
2020-07-15 11:53:52 -0500

    adding routes, controlling them through switch and render Route.js instead of AApp.js as my main component

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import { render }from 'react-dom';
 //import { Component } from 'react';
 import StorePicker from './components/StorePicker.js';
 import App from './components/App.js';
+import Router from "./components/Router.js";
 import './css/style.css';
 
 
 //render method takes two arguments(JSX,the mounting point)
-render(<App/>, document.querySelector('#main'));
+render(<Router/>, document.querySelector('#main'));
