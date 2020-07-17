80d0f8f7eea18e33432c78f72192257d11889db9
sofiamejiamuro <sofiamejiamuro@gmail.com>
2020-07-17 15:52:25 -0500

    about built-in methods and binding custom methods

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,17 +1,37 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-  render() {
+  myInput = React.createRef( ) 
+  goToStore = (event) => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get the text from the input
+    console.log(this);
+    // 3. Change the page to /store/
+    console.log('going to store');
+  };
+
+  render() {   
     return ( 
       <>
-        <form className="store-selector">
+        <form 
+          className="store-selector"
+          onSubmit={this.goToStore}
+        >
           <h2>Please Enter A Store</h2>
-          <input type="text" required placeholder="Store Name"/>
-          <button type="submit">Visit Store -> </button>
+          <input 
+            type="text" 
+            required 
+            ref={this.myInput}
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
+          <button type="submit">Visit Store --></button>
         </form>
       </>
     ) 
-  }
+  };
 }
 
-export default StorePicker;
+export default StorePicker; 
